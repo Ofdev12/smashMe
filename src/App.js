63d5014cf889793cd90fcbfc5e02d73503a602d9
frederic <olivefdev@gmail.com>
2019-03-03 18:50:40 +0000

    initial project add nav and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
+import { Button, Navbar, Alignment } from '@blueprintjs/core'
 
 export const App = () => {
-  return <div>Coucou</div>
+  return (
+    <div>
+      <Navbar fixedToTop={true} className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>SmashMe</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="map-create" text="New Game" />
+          <Navbar.Divider />
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="people" text="Team" />
+          <Button className="bp3-minimal" icon="person" text="Profile" />
+        </Navbar.Group>
+      </Navbar>
+      <div className="containerPage">
+        <h1 className="titre1">Smash Me</h1>
+      </div>
+    </div>
+  )
 }
