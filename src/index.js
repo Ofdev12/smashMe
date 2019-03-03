cecd2cdf72d25abaf108845d5f8c98b766509483
frederic <olivefdev@gmail.com>
2019-03-03 18:50:10 +0000

    set up blueprintjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+
 import { App } from './App'
 import * as serviceWorker from './serviceWorker'
 
